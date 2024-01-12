8609747f86a180eee25cdd69df4108aec2796aca
ahgddev <ahgddev@gmail.com>
2024-01-12 16:25:03 -0700

    Finished the 2nd Activity asking for a function to determine how much additional space is needed for 100 plants.

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ function growth_check(num_of_plants, weeks){
     let current_plant_amt = (num_of_plants * 2) * weeks;
     let plant_space = current_plant_amt * 0.8
 
+      //Activity 2: Rapid Growth with 100 plants
+      if (num_of_plants == 100) {
+        return "We need " + (plant_space - max_area) + " more space to plant 100 plants."
+      }
+
     if(plant_space > (max_area*.80)){
         return "Pruned"
     } else if (plant_space <= (max_area*.80) || plant_space >= (max_area*.50)) {
@@ -15,3 +20,4 @@ function growth_check(num_of_plants, weeks){
 console.log(growth_check(20,1))
 console.log(growth_check(20,2))
 console.log(growth_check(20,3))
+console.log(growth_check(100,10))
