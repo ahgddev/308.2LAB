cddf22be16c9124c6a133f21f21cbd6c1e3f8530
ahgddev <ahgddev@gmail.com>
2024-01-12 16:31:49 -0700

    Added an additional part to the message calculating the radius of 100 plants to Activity 2.

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 function growth_check(num_of_plants, weeks){
     const max_area = 78.5375;
+    const PI = 3.1415;
     let current_plant_amt = (num_of_plants * 2) * weeks;
     let plant_space = current_plant_amt * 0.8
 
       //Activity 2: Rapid Growth with 100 plants
       if (num_of_plants == 100) {
-        return "We need " + (plant_space - max_area) + " more space to plant 100 plants."
+        return "We need " + (plant_space - max_area) + " more space to plant 100 plants. The radius of this new area would be " + (Math.sqrt(plant_space)/PI)
       }
 
-    if(plant_space > (max_area*.80)){
-        return "Pruned"
-    } else if (plant_space <= (max_area*.80) || plant_space >= (max_area*.50)) {
-        return "Monitored"
-    } else {
-        return "Planted"
+    try{
+        if(plant_space > (max_area*.80)){
+            return "Pruned"
+        } else if (plant_space <= (max_area*.80) || plant_space >= (max_area*.50)) {
+            return "Monitored"
+        } else {
+            return "Planted"
+        }
+    } catch (){
+        
     }
+  
 }
 
 console.log(growth_check(20,1))
