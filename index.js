18cfe9647c1ce1570476f588007f55447e99e95c
ahgddev <ahgddev@gmail.com>
2024-01-12 16:52:36 -0700

    Finished activity 3. Added try/catch block to catch planting beyond the max area.

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ function growth_check(num_of_plants, weeks){
     let current_plant_amt = (num_of_plants * 2) * weeks;
     let plant_space = current_plant_amt * 0.8
 
-      //Activity 2: Rapid Growth with 100 plants
+    //Activity 3: try/catch block
+    try{
+        //Activity 2: Rapid Growth with 100 plants
       if (num_of_plants == 100) {
         return "We need " + (plant_space - max_area) + " more space to plant 100 plants. The radius of this new area would be " + (Math.sqrt(plant_space)/PI)
-      }
-
-    try{
-        if(plant_space > (max_area*.80)){
+        } else if (plant_space > max_area){
+            throw new Error("This is beyond the maximum garden area!")
+        }
+        else if (plant_space > (max_area*.80)){
             return "Pruned"
         } else if (plant_space <= (max_area*.80) || plant_space >= (max_area*.50)) {
             return "Monitored"
         } else {
             return "Planted"
         }
-    } catch (){
-        
+    } catch (Error){
+        console.error(Error)
     }
   
 }
